735bfa6a4682b6e506dc454c81adfb5448ca66c9
Nathan J. Russell <nathan@blackpathway.com>
2024-03-31 16:07:17 -0400

    added macro to toggle between malloc and mymalloc

--- main.c
+++ main.c
@@ -3,31 +3,40 @@
 #include <stdint.h>
 #include <string.h> // Include string.h for strcpy
 #include <sys/mman.h>
-#include "memory_allocator/memory_allocator.h"
 
+
+#ifdef USE_MY_MALLOC
+    #define MALLOC mymalloc
+    #define FREE myfree
+    #include "memory_allocator/memory_allocator.h"
+#else
+    #define MALLOC malloc
+    #define FREE free
+#endif
 
 int main() {
-    initialize_memory_pool(1024);
 
-    // Allocate memory
-    int *ptr1 = (int*)mymalloc(sizeof(int));
+#ifdef USE_MY_MALLOC
+    initialize_memory_pool(1024);
+#endif
+
+
+    int *ptr1 = (int*)MALLOC(sizeof(int));
     *ptr1 = 10;
 
-    // Allocate memory again
-    char *ptr2 = (char*)mymalloc(50 * sizeof(char));
+    char *ptr2 = (char*)MALLOC(50 * sizeof(char));
     strcpy(ptr2, "Hello, world!");
 
-    // Free memory
-    myfree(ptr1);
+    FREE(ptr1);
 
-    // Allocate memory again
-    double *ptr3 = (double*)mymalloc(sizeof(double));
+
+    double *ptr3 = (double*)MALLOC(sizeof(double));
     *ptr3 = 3.14;
 
-    // Display allocated blocks
+    #ifdef USE_MY_MALLOC
     display_blocks();
-
     destroy_memory_pool();
+    #endif
 
     return 0;
 }
--- makefile
+++ makefile
@@ -3,9 +3,13 @@ CFLAGS = -Wall -g -Imemory_allocator
 LDFLAGS = -lm
 
 OBJS = main.o memory_allocator/memory_allocator.o
+MYMALLOC_OBJS = main_mymalloc.o memory_allocator/memory_allocator.o
 
 main.o: main.c memory_allocator/memory_allocator.h
 	$(CC) $(CFLAGS) -c main.c
+
+main_mymalloc.o: main.c memory_allocator/memory_allocator.h
+	$(CC) $(CFLAGS) -DUSE_MY_MALLOC -c main.c -o main_mymalloc.o
 
 memory_allocator/memory_allocator.o: memory_allocator/memory_allocator.c memory_allocator/memory_allocator.h
 	$(CC) $(CFLAGS) -c memory_allocator/memory_allocator.c -o memory_allocator/memory_allocator.o
@@ -13,8 +17,14 @@ memory_allocator/memory_allocator.o: memory_allocator/memory_allocator.c memory_
 main: $(OBJS)
 	$(CC) $(CFLAGS) $(OBJS) -o main $(LDFLAGS)
 
+main_mymalloc: $(MYMALLOC_OBJS)
+	$(CC) $(CFLAGS) $(MYMALLOC_OBJS) -o main_mymalloc $(LDFLAGS)
+
 clean:
-	rm -f *.o memory_allocator/*.o main
+	rm -f *.o memory_allocator/*.o main main_mymalloc
 
 test: clean main
 	./main
+
+test_mymalloc: clean main_mymalloc
+	./main_mymalloc
